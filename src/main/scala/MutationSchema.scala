da94ea1fe812f5343a21b0b27eb51d87d622d4e9
tammosminia <tammosminia@gmail.com>
2021-10-29 18:08:34 +0200

    gqlType

--- src/main/scala/MutationSchema.scala
+++ src/main/scala/MutationSchema.scala
@@ -1,87 +1,48 @@
-import sangria.macros.derive.{
-  InputObjectTypeName,
-  ObjectTypeName,
-  deriveInputObjectType,
-  deriveObjectType
-}
-import sangria.marshalling.{
-  CoercedScalaResultMarshaller,
-  FromInput,
-  ResultMarshaller
-}
+import io.circe.{Decoder, HCursor}
+import io.circe.generic.auto._
+import sangria.macros.derive.{InputObjectTypeName, ObjectTypeName, deriveInputObjectType, deriveObjectType}
+import sangria.marshalling.circe._
 import sangria.schema.{Field, fields, _}
-//import io.circe.generic.auto._
-import sangria.ast
-import sangria.ast.{ObjectValue, StringValue}
-//import sangria.marshalling.circe._
 import sangria.validation.Violation
-import sangria.marshalling.RawResultMarshaller
-import java.time.LocalDate
+
 import scala.util.{Failure, Success, Try}
 
-case class Color(c: String) extends AnyVal
+case class Color(underlying: String) extends AnyVal
 object Color {
-//  implicit val gqlType: ScalarType[Color] = ScalarType[Color](
-//    "Color",
-//    description = Some("Color"),
-//    coerceOutput = (c, _) => c.c,
-////    coerceOutput = (dt, _) => ObjectValue(
-////        "c" -> StringValue(dt.c)
-////    ),
-//    coerceInput = {
-//      case StringValue(s, _, _, _, _) => parseColor(s)
-//      case _                          => Left(ParseColorViolation)
-//    },
-//    coerceUserInput = {
-//      case s: String => parseColor(s)
-//      case _         => Left(ParseColorViolation)
-//    }
-//  )
-  private def parseColor(c: String) =
-    Try(Color(c)) match {
-      case Success(v) => Right(v)
-      case Failure(_) => Left(ParseColorViolation)
-    }
-  case object ParseColorViolation extends Violation {
-    override def errorMessage: String = "Error while parsing Color"
-  }
-
-  implicit val colorFromInput: FromInput[Color] = new FromInput[Color] {
-    override val marshaller: ResultMarshaller =
-      CoercedScalaResultMarshaller.default
-
-    override def fromResult(node: marshaller.Node): Color = {
-      Color(node.asInstanceOf[String])
-    }
-
-  }
+  implicit val circeDecoder: Decoder[Color] = (c: HCursor) => c.as[String].map(Color.apply)
   implicit val gqlType: ScalarAlias[Color, String] =
-    ScalarAlias[Color, String](StringType, _.c, c => Right(Color(c)))
+    ScalarAlias[Color, String](StringType, _.underlying, c => Right(Color(c)))
 }
 
-case class Cat(name: String, color: Color)
-object Cat {
-  implicit val gqlType: ObjectType[Unit, Cat] =
-    deriveObjectType[Unit, Cat](ObjectTypeName("Cat"))
-
-  implicit val gqlInputType: InputObjectType[Cat] =
-    deriveInputObjectType[Cat](InputObjectTypeName("CatInput"))
-
-  implicit val catFromInput: FromInput[Cat] = new FromInput[Cat] {
-    override val marshaller: ResultMarshaller =
-      CoercedScalaResultMarshaller.default
-
-    override def fromResult(node: marshaller.Node): Cat = {
-      val map = node.asInstanceOf[Map[String, Any]]
-      Cat(map("name").asInstanceOf[String], map("color").asInstanceOf[Color])
-    }
-
+case class Speed(underlying: Int) extends AnyVal
+object Speed {
+  def fromInt(i: Int): Speed = {
+    require(i > 0, "cats only go forwards!")
+    Speed(i)
   }
 
+  implicit val circeDecoder: Decoder[Speed] = (c: HCursor) => c.as[Int].map(fromInt)
+  implicit val gqlType: ScalarAlias[Speed, Int] = {
+    val renamed = IntType.copy(name = "Speed", description = Some("Speed in m/s"))
+    ScalarAlias(renamed, _.underlying, parse)
+  }
+
+  private def parse(input: Int): Either[SpeedParseViolation, Speed] =
+    Try(fromInt(input)) match {
+      case Success(t) => Right(t)
+      case Failure(e) => Left(new SpeedParseViolation(s"error parsing speed: ${e.getMessage}"))
+    }
+
+  private class SpeedParseViolation(val errorMessage: String) extends Violation
+}
+
+case class Cat(name: String, color: Color, speed: Speed)
+object Cat {
+  implicit val gqlType: ObjectType[Unit, Cat] = deriveObjectType[Unit, Cat](ObjectTypeName("Cat"))
+  implicit val gqlInputType: InputObjectType[Cat] = deriveInputObjectType[Cat](InputObjectTypeName("CatInput"))
 }
 
 object MutationSchema {
-
   def mutationType: ObjectType[Unit, Unit] = ObjectType(
     "Mutation",
     fields[Unit, Unit](
@@ -90,12 +51,14 @@ object MutationSchema {
         Cat.gqlType,
         arguments = List(
           Argument("cat", Cat.gqlInputType),
-          Argument("color", Color.gqlType)
+          Argument("color", Color.gqlType),
+          Argument("speed", Speed.gqlType)
         ),
-        resolve = c => changeCat(c.arg[Cat]("cat"), c.arg[Color]("color"))
+        resolve = c => paintCat(c.arg[Cat]("cat"), c.arg[Color]("color"), c.arg[Speed]("speed"))
       )
     )
   )
 
-  def changeCat(cat: Cat, color: Color): Cat = cat.copy(color = color)
+  def paintCat(cat: Cat, color: Color, speed: Speed): Cat =
+    cat.copy(color = color, speed = speed)
 }
