f2d73a3251838aa8cb584566c9b3b15873d510f9
tammosminia <tammosminia@gmail.com>
2021-11-02 14:09:55 +0100

    gqlType

--- src/main/scala/MutationSchema.scala
+++ src/main/scala/MutationSchema.scala
@@ -1,17 +1,13 @@
-import io.circe.{Decoder, HCursor}
+import io.circe.Decoder
 import io.circe.generic.auto._
 import sangria.macros.derive.{InputObjectTypeName, ObjectTypeName, deriveInputObjectType, deriveObjectType}
 import sangria.marshalling.circe._
 import sangria.schema.{Field, fields, _}
-import sangria.validation.Violation
-
-import scala.util.{Failure, Success, Try}
 
 case class Color(underlying: String) extends AnyVal
 object Color {
-  implicit val circeDecoder: Decoder[Color] = (c: HCursor) => c.as[String].map(Color.apply)
-  implicit val gqlType: ScalarAlias[Color, String] =
-    ScalarAlias[Color, String](StringType, _.underlying, c => Right(Color(c)))
+  implicit val circeDecoder: Decoder[Color] = SangriaUtils.circeValueClassDecoder(apply)
+  implicit val gqlType: ScalarAlias[Color, String] = SangriaUtils.gqlAliasType(apply, _.underlying)
 }
 
 case class Speed(underlying: Int) extends AnyVal
@@ -21,19 +17,9 @@ object Speed {
     Speed(i)
   }
 
-  implicit val circeDecoder: Decoder[Speed] = (c: HCursor) => c.as[Int].map(fromInt)
-  implicit val gqlType: ScalarAlias[Speed, Int] = {
-    val renamed = IntType.copy(name = "Speed", description = Some("Speed in m/s"))
-    ScalarAlias(renamed, _.underlying, parse)
-  }
-
-  private def parse(input: Int): Either[SpeedParseViolation, Speed] =
-    Try(fromInt(input)) match {
-      case Success(t) => Right(t)
-      case Failure(e) => Left(new SpeedParseViolation(s"error parsing speed: ${e.getMessage}"))
-    }
-
-  private class SpeedParseViolation(val errorMessage: String) extends Violation
+  implicit val circeDecoder: Decoder[Speed] = SangriaUtils.circeValueClassDecoder(fromInt)
+  implicit val gqlType: ScalarAlias[Speed, Int] =
+    SangriaUtils.gqlValueClassType[Speed, Int](fromInt, _.underlying, "Speed in m/s")
 }
 
 case class Cat(name: String, color: Color, speed: Speed)
---
+++ src/main/scala/SangriaUtils.scala
@@ -0,0 +1,46 @@
+import io.circe.{Decoder, HCursor}
+import sangria.schema.{ScalarAlias, ScalarType}
+import sangria.validation.Violation
+
+import scala.reflect.{classTag, ClassTag}
+import scala.util.{Failure, Success, Try}
+
+object SangriaUtils {
+
+  /** Needed at runtime to correctly decode value classes. */
+  def circeValueClassDecoder[T, U: Decoder](decode: U => T): Decoder[T] = (c: HCursor) => c.as[U].map(decode)
+
+  /** Create a gql type for value class `T`, that will be an alias for an existing gqlType `U`.
+    * It will still be represented as `U` by graphQl.
+    */
+  def gqlAliasType[T: ClassTag, U](decode: U => T, encode: T => U)(implicit
+      d: Decoder[T],
+      uType: ScalarType[U]
+  ): ScalarAlias[T, U] =
+    ScalarAlias[T, U](uType, encode, gqlParse(decode))
+
+  /** Parse input of gqlType `U` into a value class `T`.
+    * It will be represented as `className[T]` by graphQl.
+    */
+  def gqlValueClassType[T: ClassTag, U](
+      decode: U => T,
+      encode: T => U,
+      description: String
+  )(implicit d: Decoder[T], uType: ScalarType[U]): ScalarAlias[T, U] = {
+    val renamed = uType.copy(name = className[T], description = Some(description))
+    ScalarAlias[T, U](renamed, encode, gqlParse(decode))
+  }
+
+  private def gqlParse[T: ClassTag, U](decode: U => T)(input: U): Either[GqlParseViolation[T], T] =
+    Try(decode(input)) match {
+      case Success(t) => Right(t)
+      case Failure(_) => Left(new GqlParseViolation[T])
+    }
+
+  private class GqlParseViolation[T: ClassTag] extends Violation {
+    override def errorMessage: String = s"Error while parsing ${className[T]}"
+  }
+
+  //Include the package to make sure names are unique
+  private def className[T: ClassTag]: String = classTag[T].runtimeClass.getName.replace(".", "_").replace("$", "_")
+}
